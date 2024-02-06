6390e323e9b018305a8f4a3d43b85ac155b06ddf
brijesh2004 <brskumar0102@gmail.com>
2024-02-06 12:19:30 +0530

    Choose Number Game

--- src/components/Failed.tsx
+++ src/components/Failed.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { useRouter } from 'next/navigation';
 import style from '@/app/style/level.module.css';
 
 const Failed = ({level}:any) => {
-    const router = useRouter();
   return (
     <div>
       <h1>Better Luck Next Time 😪</h1>
