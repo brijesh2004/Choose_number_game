ee60b3739817df73ccaca59494e690ffeb25f3de
brijesh2004 <brskumar0102@gmail.com>
2024-02-06 13:21:00 +0530

    Choose Number Game

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 import { useRouter } from "next/navigation";
 import style from '@/app/style/level.module.css'
+import Head from "next/head";
 export default function Home() {
   const router = useRouter();
   return (
    <div className={style.main}> 
+    <Head>
+        <link rel="icon" href="/fav.jpeg" />
+      </Head>
     <h1>Hey Welcome to the Choose Greatest Number Game</h1>
    <button className={style.btn}
    onClick={()=>router.push('/level')}>Start Playing</button>
